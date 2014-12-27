819084b3a3d86400a8871027e171873471ff9e2f
mil <miles.sandlar@gmail.com>
2014-12-26 19:57:15 -0500

    filter by

--- userbound.com_src/_js/new_onload.js
+++ userbound.com_src/_js/new_onload.js
@@ -6,10 +6,12 @@ var UserboundInterface = (function(my) {
 
     $(".filter-by button").on("click", function(e) {
       var filter_by = e.target.innerHTML.toLowerCase();
+      $(".filter-by button").removeClass("active");
 
       $(".interfaces-list a").each(function(i, el) {
         if (($(el).attr("data-category-" + filter_by) != null) || filter_by == "all") {
           $(el).css("display", "block");
+          $(e.target).addClass("active");
 
         } else {
           $(el).css("display", "none");
--- userbound.com_src/blog/entries/2014-12-25-Gulp-for-Sitebuilds.md
+++ userbound.com_src/blog/entries/2014-12-25-Gulp-for-Sitebuilds.md
@@ -4,4 +4,4 @@ categories: blog
 date: 2014-12-25
 time: 4:00PM
 ---
-I've decided to switch my blog over from Jekyll to Gulp. The reason `why`
+
