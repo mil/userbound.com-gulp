3fe4ca48e78006e9d6fe9e3e8aafd230c0f4866c
mil <miles.sandlar@gmail.com>
2015-01-02 14:02:22 -0500

    proper popstate with html5 history

--- src/_js/new_onload.js
+++ src/_js/new_onload.js
@@ -12,15 +12,72 @@ function smoothScroll(el, to, duration) {
     }.bind(this), 10);
 }
 
+var undo_stack = [];
+var redo_stack = [];
+
+
+function trigger_state(state) {
+  window.history.pushState(null, null, state.href);
+  state.cb();
+}
+function push_state(state) {
+  var old_section = current_active_section();
+  console.log(old_section);
+  undo_stack.push({
+    href: window.location.pathname,
+    cb:   function() { 
+      activate_subsection(old_section); 
+    }
+  });
+  trigger_state(state);
+}
+function pop_state() {
+  if (!undo_stack.length) { window.history.back(); }
+  trigger_state(undo_stack[undo_stack.length - 1]);
+  undo_stack.pop();
+}
+
+function current_active_section() {
+  return $(".filter-by button.active").text().toLowerCase();
+}
+
+function activate_subsection(subsection) {
+  $(".filter-by button").removeClass("active");
+  $(".filter-by button").each(function(i, el) {
+    if ($(el).text().toLowerCase() == subsection) {
+      $(el).addClass("active");
+    }
+  });
+
+  var out_els = [], in_els = [];
+  $(".filter-el").each(function(i, el) { 
+    if (
+      ($(el).attr("data-category-" + subsection) != null) || 
+          (subsection == "all")
+    ) { in_els.push(el); } else { out_els.push(el); }
+  });
+
+  $(out_els).animate({ opacity: 0 }, function() {
+    $("header").after(in_els);
+    $(in_els).animate({ opacity : 1 }, function() {
+      $(in_els).addClass("visible");
+      $(out_els).removeClass("visible");
+    });
+  });
+}
+
+
+
 
 var UserboundInterface = (function(my) {
   my.init = function() {
     //$(window).on("unload", function() {
     //});
-    //
+    $(window).bind("popstate", pop_state);
+
     $("a").on("click", function(e) {
         e.preventDefault();
-var new_href = $($(e.target).is("a") ? 
+        var new_href = $($(e.target).is("a") ? 
           $(e.target) : $(e.target).closest("a")
          ).attr("href");
 
@@ -38,43 +95,15 @@ var new_href = $($(e.target).is("a") ?
       }, 100);
     });
     
-
+ 
     $(".filter-by button").on("click", function(e) {
+      var subsection = e.target.innerHTML.toLowerCase()
+
       e.preventDefault();
-      var filter_by = e.target.innerHTML.toLowerCase();
-      $(".filter-by button").removeClass("active");
-      $(e.target).addClass("active");
-
-      $(".filter-el").each(function(i, el) {
-        var out_els = [];
-        var in_els = [];
-
-        window.history.pushState(null, null, "/tessft");
-        if (
-          ($(el).attr("data-category-" + filter_by) != null) || 
-          (filter_by == "all")
-        ) { in_els.push(el); } else { out_els.push(el); }
-
-
-        $(out_els).animate({
-          opacity: 0
-        }, function() {
-          $("header").after(in_els);
-          
-
-          $(in_els).css("display", "block").animate({
-            opacity : 1
-          }, function() {
-            $(in_els).addClass("visible");
-            $(out_els).css("display", "none").removeClass("visible");
-          });
-
-
-        });
-
-
-
-      });
+      push_state({
+        href: "/" + $("h1").text().toLowerCase() + "/" + subsection,
+        cb: function() { activate_subsection(subsection); },
+      });  
     });
 
     sh_highlightDocument();
@@ -86,4 +115,3 @@ var new_href = $($(e.target).is("a") ?
 
 // Page load
 $(function($) { UserboundInterface.init(); });
-
