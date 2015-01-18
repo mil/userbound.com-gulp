560b715b36c015bba056022fef96dc11ca205e85
mil <miles.sandlar@gmail.com>
2015-01-18 14:02:32 -0500

    overhaul frontendrouting using pushstate grapnel

--- src/_js/new_onload.js
+++ src/_js/new_onload.js
@@ -1,186 +1,106 @@
-function smoothScroll(el, to, duration) {
-    if (duration < 0) {
-        return;
-    }
-    var difference = to - $(window).scrollTop();
-    var perTick = difference / duration * 10;
-    this.scrollToTimerCache = setTimeout(function() {
-        if (!isNaN(parseInt(perTick, 10))) {
-            window.scrollTo(0, $(window).scrollTop() + perTick);
-            smoothScroll(el, to, duration - 10);
-        }
-    }.bind(this), 10);
-}
+var UserboundInterface = (function(my) {
+  var router = new Grapnel({ pushState: true });
 
-var undo_stack = [];
-var redo_stack = [];
+  function activate_subsection(subsection) {
+    if (current_active_section() == subsection) { return; }
 
-function update_head_rotation() {
-  return;
-  var total   = $("body").offset().height / 2;
-  var scroll  = $(window).scrollTop();
-  var percent = scroll / total;
-
-  var head_min_rotation = 0;
-  var head_max_rotation = 20;
-  var range = Math.abs(head_max_rotation - head_min_rotation);
-  var head_position = Math.max(Math.min(
-      head_max_rotation - parseInt(percent * range),
-      head_max_rotation
-  ), head_min_rotation);
-
-  $(".guy .head").css(
-    'transform', 'rotate(' + head_position + 'deg)'
-  );
-}
-
-function home_animation() {
-  if (!$(".home").length) { return; }
-
-  function cb() {
-
-    
-    var n = $(".guy").parent().index();
-    while (n == $(".guy").parent().index()) {
-      n = $(".home a")[parseInt(Math.random()*4)]; 
-    }
+    var active_subsection_el  = $(".filter-el.visible");
+    var active_subsection_btn = $(".filter-by button");
+    var out_els = [], in_els = [];
 
 
-
-
-    $(".guy").animate({
-      'margin-top' : "50px",
-      'opacity' : 0
-    }, {
-      duration: 2000,
-      complete: function() {
-
-        $(n).prepend($(".guy"));
-        $(".guy").animate({
-          'margin-top' : "-60px",
-          'opacity' : 1
-        }, 2000, function() {
-          setTimeout(cb, 5000);
-        });
+    active_subsection_btn.removeClass("active");
+    $(".filter-by button").each(function(i, el) {
+      if ($(el).text().toLowerCase() == subsection) {
+        $(el).addClass("active");
       }
     });
 
+
+    active_subsection_el.animate({ opacity: 0 }, function() {
+      var new_subsection_el = 
+        $(".filter-el[data-category-" + subsection + "]");
+      active_subsection_el.removeClass("visible");
+      new_subsection_el.css("opacity", 0).addClass("visible");
+      new_subsection_el.animate({ opacity: 1 }, function() {});
+    });
   }
 
-  $(".guy").css("position", "absolute");
-  $(".guy").css("margin-top", "100px");
-  $(".guy").css("display", "inline-block");
-  $(".guy").css("width", "18px");
-  //setInterval(cb, 2000);
-  cb();
- 
-}
+  function current_active_section() {
+    return $(".filter-by button.active").text().toLowerCase();
+  }
 
 
-function trigger_state(state) {
-  window.history.pushState(null, null, state.href);
-  state.cb();
-}
-function push_state(state) {
-  var old_section = current_active_section();
-  undo_stack.push({
-    href: window.location.pathname,
-    cb:   function() { 
-      activate_subsection(old_section); 
-    }
-  });
-  trigger_state(state);
-}
-function pop_state() {
-  if (!undo_stack.length) { window.history.back(); }
-  trigger_state(undo_stack[undo_stack.length - 1]);
-  undo_stack.pop();
-}
+  function link_click(e) {
+    if ($(e.target).attr("target") === "_blank") { return; }
 
-function current_active_section() {
-  return $(".filter-by button.active").text().toLowerCase();
-}
+    var scroll_speed_ms  = 100; 
+    var css_animation_ms = 1000;
+    var target_link      = $(e.target).is("a") ? $(e.target) : $(e.target).closest("a");
+    var load_new_page_fn = function() { 
+      window.location = $(target_link).attr('href'); 
+    };
 
-function activate_subsection(subsection) {
-  $(".filter-by button").removeClass("active");
-  $(".filter-by button").each(function(i, el) {
-    if ($(el).text().toLowerCase() == subsection) {
-      $(el).addClass("active");
-    }
-  });
-
-  var out_els = [], in_els = [];
-  var x = $(".filter-el.visible");
-
-  $(".filter-el.visible").animate({
-    opacity: 0
-  }, function() {
-
-
-    var i = $(".filter-el[data-category-" + subsection + "]");
-
-    x.removeClass("visible");
-    i.css("opacity", 0).addClass("visible");
-
-    i.animate({
-      opacity: 1
-    }, function() {});
-  });
-}
-
-
-function link_click(e) {
-  if ($(e.target).attr("target") === "_blank") { return; }
     e.preventDefault();
-    var new_href = $($(e.target).is("a") ? 
-      $(e.target) : $(e.target).closest("a")
-     ).attr("href");
+    smooth_scroll($(window), 0, scroll_speed_ms);
+    setTimeout(function() {
+      $("nav").addClass('fade-out');
+      $("main").addClass('fade-up');
+      setTimeout(load_new_page_fn, css_animation_ms);
+    }, scroll_speed_ms);
+  }
+ 
+  function smooth_scroll(el, to, duration) {
+    // adapted from: http://austinpray.com/blog/zepto-js-smooth-vertical-scrolling/ 
+    if (duration < 0) { return; }
+    var difference = to - $(window).scrollTop();
+    var perTick = difference / duration * 10;
+    this.scrollToTimerCache = setTimeout(function() {
+      if (!isNaN(parseInt(perTick, 10))) {
+        window.scrollTo(0, $(window).scrollTop() + perTick);
+        smooth_scroll(el, to, duration - 10);
+      }
+    }.bind(this), 10);
+  }  
 
+  function subsection_button_click(e) {
+    var subsection = e.target.innerHTML.toLowerCase();
 
-  smoothScroll($(window), 0, 100);
+    // Models follows a different schema since its only subpage with stubs
+    var new_href = (window.location.href.match("/models")) ?
+      "/models/" + $("h1").text() + "/" + subsection :
+        "/" + $("h1").text().toLowerCase() + "/" + subsection; 
 
-  setTimeout(function() {
-    
-    $("nav").addClass('fade-out');
-    $("main").addClass('fade-up');
+    e.preventDefault();
+    router.navigate(new_href);
+  }
+  
 
-    setTimeout(function() { window.location = new_href; }, 1000);
-  }, 100);
-}
-function filter_click(e) {
-  var subsection = e.target.innerHTML.toLowerCase()
+  return { 
+    init: function() {
+      // Setup click callbacks for links and subsection clicking
+      $("a").on("click", link_click);
+      $(".filter-by button").on("click", subsection_button_click);
+      $("img[data-category-model]").on("click", $($(".filter-by button")[1]).click);
 
-  // Models follows a different schema since its only subpage with stubs
-  var new_href = (window.location.href.match("/models")) ?
-    "/models/" + $("h1").text() + "/" + subsection :
-    "/" + $("h1").text().toLowerCase() + "/" + subsection; 
+      // Initialize routing with Grapnel
+      ['about', 'interfaces', 'models/:section'].forEach(function(section) {
+        router.get('/' + section, function(request) {
+          activate_subsection(
+            $($(".filter-by button")[0]).text().toLowerCase()
+          );   
+        });
+        router.get('/' + section + '/:subsection', function(request) {
+          var subsection = request.params.subsection;
+          activate_subsection(subsection);   
+        });
+      });
 
-  e.preventDefault();
-  push_state({
-    href: new_href,
-    cb: function() { activate_subsection(subsection); },
-  });  
-}
-
-
-var UserboundInterface = (function(my) {
-  my.init = function() {
-    //$(window).on("unload", function() {});
-    $(window).on("popstate", pop_state);
-    $(window).on("scroll", update_head_rotation);
-    $("a").on("click", link_click);
-    $(".filter-by button").on("click", filter_click);
-    $("img[data-category-model]").on("click", function() {
-      $($(".filter-by button")[1]).click();
-    });
-
-    update_head_rotation();
-    //home_animation();
-    sh_highlightDocument();
+      // Enable syntax highlighting with sh_ classes on <pre>'s
+      sh_highlightDocument();
+    }
   };
-
-  return my;
 })(UserboundInterface || {});
 
 // Page load
-$(function($) { UserboundInterface.init(); });
+$(UserboundInterface.init);
