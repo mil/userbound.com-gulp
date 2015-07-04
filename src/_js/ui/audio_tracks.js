4b58da928bf0a193f0cf252d3f122df1f1e81521
mil <miles.sandlar@gmail.com>
2015-07-04 14:25:25 -0500

    c

--- src/_js/ui/audio_tracks.js
+++ src/_js/ui/audio_tracks.js
@@ -3,7 +3,8 @@ window.UserboundInterface = (function(self) {
   return $.extend(self, {
     tracks: {
       '6/18/15': 'db049.WAV',
-      '6/17/15': 'db048.WAV'
+      '6/17/15': 'db048.WAV',
+      '6/16/15': 'db047.WAV'
     }
   });
 })(UserboundInterface || {});
